af59d373c7e0810e27ae2abeadfb4c2cc8d0c277
HASAN-Ammar <73344114+HASAN-Ammar@users.noreply.github.com>
2025-05-20 15:26:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
 from sksurv.datasets import get_x_y
 from sksurv.ensemble import RandomSurvivalForest
