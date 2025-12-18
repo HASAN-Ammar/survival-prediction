2db1120a5dabc70a8246b3d68358f4ea686abbe2
HASAN-Ammar <73344114+HASAN-Ammar@users.noreply.github.com>
2025-12-18 21:55:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ col1, col2 = st.columns(2)
 Largest_nodule_diameter = col1.number_input("Size of the largest nodule in mm", min_value=1, value= 10, placeholder="Size in millimetes")
 Satellite_nodules = col1.number_input("Satellite nodules", min_value=0, max_value=1, value=0, placeholder= "0/1")
 VETC_subtype = col1.number_input("VETC subtype", min_value=0, max_value=1, value=0, placeholder= "0/1")
-Macrotrabecular_massive_subtype = col2.number_input("Macrotrabecular massive subtype", min_value=0, max_value=1, value=0, placeholder= "0/1")
+Surgical_margins = col2.number_input("Surgical margins", min_value=0, max_value=1, value=0, placeholder= "0/1")
 Microvascular_invasion = col2.number_input("Microvascular invasion", min_value=0, max_value=1, value=0, placeholder= "0/1")
-BCLC_before_intervention = col1.number_input(    "BCLC before surgery (0, A=1, B=2, C=3)", min_value=0,max_value=3, value=0, placeholder= "0-3")
+BCLC_before_intervention = col1.number_input("BCLC before surgery (0, A=1, B=2, C=3)", min_value=0,max_value=3, value=0, placeholder= "0-3")
 Edmondson_Steiner_Grade = col1.number_input("Edmondson steiner grade (0=I, II=1, III=2, IV=3, V=4)", min_value=0, max_value=4, value=0, placeholder= "0-4")
-Number_of_tumors_on_the_specimen = col2.number_input("Number of tumors on the specimen", min_value=0, max_value=4, value=0, placeholder= "0-4")
+Number_of_tumors_on_the_specimen = col2.number_input("Number of tumors on the specimen", min_value=1, max_value=10, value=1, placeholder= "1-10")
 Preop_AFP = col2.number_input("Preoperative AFP level (ng/ml)", min_value=0.0, value=1.0, placeholder= "AFP level (ng/ml)")
-Gender = col2.number_input("Gender (Female=0, Male=1)", min_value=0, max_value=1, value=0, placeholder= "0/1")
+Cirrhosis = col2.number_input("Cirrhosis", min_value=0, max_value=1, value=0, placeholder= "0/1")
 
 df = pd.DataFrame(
     {
@@ -30,17 +30,16 @@ df = pd.DataFrame(
         "Microvascular_invasion": [Microvascular_invasion],
         "BCLC_before_intervention": [BCLC_before_intervention],
         "Preop_AFP": [Preop_AFP],
-        "Macrotrabecular_massive_subtype": [Macrotrabecular_massive_subtype],
+        "Surgical_margins": [Surgical_margins],
         "Edmondson_Steiner_Grade": [Edmondson_Steiner_Grade],
         "Number_of_tumors_on_the_specimen": [Number_of_tumors_on_the_specimen],
-        "Gender": [Gender],
+        "Cirrhosis": [Cirrhosis],
     }
 )
 
 train_data = pd.read_csv("hcc_pre_postoperative.csv")
 X, y = get_x_y(train_data, ["DFS", "DFS_Delay"], pos_label=1)
-
-rsf = RandomSurvivalForest(n_estimators=250, max_depth=6, n_jobs=-1, random_state=20)
+rsf = RandomSurvivalForest(n_estimators=250, max_depth=5, min_samples_leaf=1,min_samples_split=2, n_jobs=-1, random_state=20)
 rsf.fit(X[df.columns], y)
 # Predict survival function for the new data
 survival_rsf = rsf.predict_survival_function(df)
@@ -66,4 +65,5 @@ if st.button("Generate Plot"):
         y_label="Survival Probability",
         color=["#FF0000"],
     )
+
 
